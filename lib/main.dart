c1f8cb7d342cc637bfefa36cd23a79ec4fb3e3fc
mdrubel117 <114472588+mdrubel117@users.noreply.github.com>
2024-05-29 14:16:16 +0600

    lab4

--- lib/main.dart
+++ lib/main.dart
@@ -8,55 +8,130 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Two Screen App',
+      title: 'TabView App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: FirstScreen(),
+      home: HomeScreen(),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First Screen'),
+        title: Text('TabView App'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(icon: Icon(Icons.home), text: 'Home'),
+            Tab(icon: Icon(Icons.list), text: 'List'),
+            Tab(icon: Icon(Icons.info), text: 'Info'),
+          ],
+        ),
       ),
-      body: Container(
-        color: Colors.lightBlueAccent,
-        child: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SecondScreen()),
-              );
-            },
-            child: Text('Go to Second Screen'),
-          ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          HomeTab(),
+          ListTab(),
+          InfoTab(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => DirectPage()),
+          );
+        },
+        child: Icon(Icons.navigation),
+      ),
+    );
+  }
+}
+
+class HomeTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.redAccent,
+      child: Center(
+        child: Text(
+          'Home Tab',
+          style: TextStyle(fontSize: 24, color: Colors.white),
         ),
       ),
     );
   }
 }
 
-class SecondScreen extends StatelessWidget {
+class ListTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.greenAccent,
+      child: ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('Item ${index + 1}'),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class InfoTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blueAccent,
+      child: Center(
+        child: Text(
+          'Info Tab',
+          style: TextStyle(fontSize: 24, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
+
+class DirectPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Second Screen'),
+        title: Text('Direct Page'),
       ),
       body: Container(
-        color: Colors.lightGreenAccent,
+        color: Colors.purpleAccent,
         child: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text('Back to First Screen'),
+          child: Text(
+            'Direct Page',
+            style: TextStyle(fontSize: 24, color: Colors.white),
           ),
         ),
       ),
